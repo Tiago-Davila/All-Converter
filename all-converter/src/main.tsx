47207ee8467b0cab1242f7025750ba7360f93772
Tiago-Davila <tiagodavila08@gmail.com>
2026-07-13 17:34:58 -0300

    feat(main): cablear cuelume.bind() al arranque para atributos data-cuelume-*

--- all-converter/src/main.tsx
+++ all-converter/src/main.tsx
@@ -1,6 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { bind } from 'cuelume'
 import './index.css'
 import { App } from './App'
+
+// Cablear todos los atributos data-cuelume-* del DOM (Principio: declarativo primero)
+bind()
 
 createRoot(document.getElementById('root')!).render(<StrictMode><App /></StrictMode>)
